6caa06ad20e1fb8a05ba0c89e569cea45c19fc54
Drawaes <seawardtim@gmail.com>
2017-09-21 02:01:12 +0100

    Removed an extra un-needed slice

--- src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
+++ src/Microsoft.AspNetCore.SignalR.Common/Internal/Formatters/BinaryMessageParser.cs
@@ -19,27 +19,24 @@ namespace Microsoft.AspNetCore.SignalR.Internal.Formatters
             }
 
             // Read the length
-            length = buffer.Span.Slice(0, sizeof(long)).ReadBigEndian<long>();
+            length = buffer.Span.Slice(0, sizeof(long)).ReadBigEndian<long>() + sizeof(long);
 
             if (length > Int32.MaxValue)
             {
                 throw new FormatException("Messages over 2GB in size are not supported");
             }
 
-            // Skip over the length
-            var remaining = buffer.Slice(sizeof(long));
-
             // We don't have enough data
-            while (remaining.Length < (int)length)
+            if (buffer.Length < (int)length + sizeof(long))
             {
                 return false;
             }
 
             // Get the payload
-            payload = remaining.Slice(0, (int)length);
+            payload = buffer.Slice(sizeof(long), (int)length);
 
             // Skip the payload
-            buffer = remaining.Slice((int)length);
+            buffer = buffer.Slice((int)length + sizeof(long));
             return true;
         }
     }
